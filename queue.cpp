0a81330593aead733ae3f75b7e8f75e836e7b35d
jangmuk yoo <ghve3691@naver.com>
2025-06-03 20:50:10 +0900

    enqueue, dequeue 힙으로 구현

--- queue.cpp
+++ queue.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "queue.h"
 
+using namespace std;
+
 inline int parent(int i) { return (i - 1) / 2; }
 inline int left(int i) { return 2 * i + 1; }
 inline int right(int i) { return 2 * i + 2; }
@@ -37,13 +39,52 @@ Node* nclone(Node* node) {
 
 
 Reply enqueue(Queue* queue, Item item) {
-	
+	lock_guard<mutex> lock(queue->lock);
 
+	if (queue->size >= MAX_SIZE) {
+		return { false, item };
+	}
+
+	int i = queue->size++;
+	queue->data[i] = item;
+	while(i > 0 && queue->data[parent(i)].key < queue->data[i].key) {
+		// 부모 노드와 비교하여 우선순위가 낮으면 교환
+		swap(queue->data[i], queue->data[parent(i)]);
+		i = parent(i);
+	}
+
+	return { true, item };
 }
 
 Reply dequeue(Queue* queue) {
-	Reply reply = { false, NULL };
-	return reply;
+	lock_guard<mutex> lock(queue->lock);
+
+	if(queue->size == 0) {
+		return { false, {0, nullptr} };
+	}
+
+	Item top = queue->data[0];
+	queue->data[0] = queue->data[--queue->size];
+
+	int i = 0;
+	while(true) {
+		int largest = i;
+		int leftNode = left(i);
+		int rightNode = right(i);
+		if(leftNode < queue->size && queue->data[leftNode].key > queue->data[largest].key) {
+			largest = leftNode;
+		}
+		if(rightNode < queue->size && queue->data[rightNode].key > queue->data[largest].key) {
+			largest = rightNode;
+		}
+		if(largest == i) {
+			break; // 더 이상 교환할 필요 없음
+		}
+		swap(queue->data[i], queue->data[largest]);
+		i = largest;
+	}
+
+	return { true, top };
 }
 
 Queue* range(Queue* queue, Key start, Key end) {
