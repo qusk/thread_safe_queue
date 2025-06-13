bcbcb9a85b17d9344497680ad1efb47c9a536d21
jangmuk yoo <ghve3691@naver.com>
2025-06-13 11:52:17 +0900

    복사, 정렬, 삽입의 작업을 간단하게 처리하기 위한 temp 구조체(원본 큐의 최소한의 정보를 저장) 추가

--- queue.cpp
+++ queue.cpp
@@ -16,7 +16,7 @@ Queue* init(void) {
 
 void release(Queue* queue) {
 	{
-		lock_guard<mutex> lock(queue->lock);
+		lock_guard<mutex> guard(queue->lock);
 
 		Node* current = queue->head;
 		while(current) {
@@ -63,19 +63,21 @@ Node* nclone(Node* node) {
 
 
 Reply enqueue(Queue* queue, Item item) {
-
+	// 깊은 복사
 	void* value_copy = malloc(item.value_size);
 	if(!value_copy) return {false, item};
 	memcpy(value_copy, item.value, item.value_size);
 
+	// 노드 생성
 	Node* node = new Node;
 	node->item.key = item.key;
 	node->item.value = value_copy;
 	node->item.value_size = item.value_size;
 	node->next = nullptr;
 
+	// 리스트 index 추가 및 정렬, key값 중복 처리 
 	{
-		lock_guard<mutex> lock(queue->lock);
+		lock_guard<mutex> guard(queue->lock);
 
 		Node* prev = nullptr;
 		Node* current = queue->head;
@@ -110,8 +112,9 @@ Reply enqueue(Queue* queue, Item item) {
 Reply dequeue(Queue* queue) {
 	Node* node;
 	
+	// lock 안에서 head제거
 	{
-		lock_guard<mutex> lock(queue->lock);
+		lock_guard<mutex> guard(queue->lock);
 		if(!queue->head) return {false, {0, nullptr, 0}};
 
 		node = queue->head;
@@ -132,44 +135,73 @@ Reply dequeue(Queue* queue) {
 }
 
 Queue* range(Queue* queue, Key start, Key end) {
-	Queue* new_queue = new Queue;
-	new_queue->head = nullptr;
-	new_queue->tail = nullptr;
-	
-	lock_guard<mutex> lock(queue->lock);
-	Node* current = queue->head;
 
-	while(current) {
-		Key key = current->item.key;
-		if(key >= start && key <= end) {
-			void* value_copy = malloc(current->item.value_size);
-			if(!value_copy) break;
-			memcpy(value_copy, current->item.value, current->item.value_size);
+	// 원본 큐의 최소한의 정보만 담아두기 위한 Temp구조체
+	struct Temp { 
+		Key key;
+		int size;
+		void* ptr;
+	};
 
-			Node* node = new Node;
-			node->item.key = key;
-			node->item.value = value_copy;
-			node->item.value_size = current->item.value_size;
-			node->next = nullptr;
-
-			Node* prev = nullptr;
-			Node* new_current = new_queue->head;
-			while(new_current && new_current->item.key >= key) {
-				prev = new_current;
-				new_current = new_current->next;
-			}
-
-			if(!prev) {
-				node->next = new_queue->head;
-				new_queue->head = node;
-				if(!new_queue->tail) new_queue->tail = node;
-			} else {
-				node->next = prev->next;
-				prev->next = node;
-				if(!node->next) new_queue->tail = node;
+	// snapshot배열을 할당하기 위한 노드 몇개인지 카운트(malloc으로 할당해야해서 몇 개가 있는지 알기 위해)
+	int count = 0;
+	{
+		lock_guard<mutex> guard(queue->lock);
+		for(Node* current = queue->head; current; current = current->next) {
+			if(current->item.key >= start && current->item.key <= end) {
+				++count;
 			}
 		}
-		current = current->next;
 	}
+
+	// temp 메모리 할당
+	Temp* temp = (Temp*)malloc(sizeof(Temp) * count);
+	if(!temp) return nullptr;
+
+
+	// snapshot배열 안에 범위 안에 들어있는 해당 노드의 key, value_size, value값 복사
+	{
+		lock_guard<mutex> guard(queue->lock);
+		int index = 0;
+		for(Node* current = queue->head; current; current = current->next) {
+			if(current->item.key >= start && current->item.key <= end) {
+				temp[index++] = {current->item.key, current->item.value_size, current->item.value};
+			}
+		}
+	}
+
+
+	Queue* new_queue = init();
+	for(int i = 0; i < count; ++i) {
+		void* temp_size = malloc(temp[i].size);
+		if(!temp_size) break;
+		memcpy(temp_size, temp[i].ptr, temp[i].size);
+
+		Node* node = new Node;
+		node->item.key = temp[i].key;
+		node->item.value_size = temp[i].size;
+		node->item.value = temp_size;
+		node->next = nullptr;
+
+		Node* prev = nullptr;
+		Node* new_current = new_queue->head;
+
+		// 정렬(내림차순)
+		while(new_current && new_current->item.key >= node->item.key) {
+			prev = new_current;
+			new_current = new_current->next;
+		}
+
+		if(!prev) {
+			node->next = new_queue->head;
+			new_queue->head = node;
+			if(!new_queue->tail) new_queue->tail = node;
+		} else {
+			node->next = prev->next;
+			prev->next = node;
+			if(!node->next) new_queue->tail = node;
+		}
+	}
+	free(temp);
 	return new_queue;
 }
